e8add2f743e7244aec5eb1160b8501f45346c697
omarhr-001 <omarharbaoui987@gmail.com>
2026-06-06 03:13:39 +0100

    Meta Pixel5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata, Viewport } from 'next'
-import Script from 'next/script'
 import { Syne, DM_Sans } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import { AuthProvider } from '@/lib/auth-context'
@@ -8,7 +7,6 @@ import './globals.css'
 
 const syne = Syne({ subsets: ["latin"], variable: '--font-syne' });
 const dmSans = DM_Sans({ subsets: ["latin"], variable: '--font-dm-sans' });
-const metaPixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID || '2470196180100029'
 
 export const metadata: Metadata = {
   title: 'Hamroun Meuble & Electro - L\'électroménager de qualité',
@@ -30,29 +28,6 @@ export default function RootLayout({
   return (
     <html lang="fr" className={`${syne.variable} ${dmSans.variable}`}>
       <body className="font-sans antialiased bg-gray-50">
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${metaPixelId}');
-            fbq('track', 'PageView');
-          `}
-        </Script>
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src={`https://www.facebook.com/tr?id=${metaPixelId}&ev=PageView&noscript=1`}
-            alt=""
-          />
-        </noscript>
         <MetaPixel />
         <AuthProvider>
           {children}
